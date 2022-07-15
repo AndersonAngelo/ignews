95895449000c36f47b2fedca40749e4cb5959180
Anderson_Angelo <anderson_angelo@pop-os.localdomain>
2022-07-15 11:25:20 -0300

    added components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head  from "next/head";
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ig News | Home</title>
+        <title>Home | Ig News</title>
       </Head>
-      <h1>
-        Hello world
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>News about <br />the <span>React</span> world</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="./images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
