4683219c44f0420619e3329315055df57fb4900e
AndersonAngelo <anderson_angelo1@hotmail.com>
2022-08-22 00:32:38 -0300

    created colletions on faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,5 +11,41 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET_KEY,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+      try {
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          await fauna.query(
+            q.Create(
+              q.Collection('users'),
+              {
+                data: { email }
+              }
+            )
+          ),  
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
+          )
+        )        
+        return true;
+
+      } catch {
+
+        return false;
+      }
+    }
+  },
   secret: process.env.SECRET,
 })
